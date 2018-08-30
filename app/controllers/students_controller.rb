1341fb6575db014c0dd77537c42e5017f93b0623
github username <ariellenramirez@gmail.com>
2018-08-30 17:59:07 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
+  # before_action :student_id, only: :show
+
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def student_id
+    @student = Student.find(params[:id])
   end
 end
